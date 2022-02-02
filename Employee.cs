81a460d889e5f1e61c7b0cb349396d56792e5c9a
ZhiJun Kong <zhijunkong@microsoft.com>
2022-02-01 16:27:26 -0800

    initial draft

---
+++ Employee.cs
@@ -0,0 +1,5 @@
+public class Employee {
+    public string name {get; set;}
+    public string id {get;set;}
+    public string email {get;set;}
+}
---
+++ EmployeesRequest.cs
@@ -0,0 +1,4 @@
+using System.Collections.Generic;
+public class EmployeesRequest {
+    public List<Employee> employees {get;set;}
+}
